d40274194d818d5994af5bd71bc1268b4b65d3d0
Nitya Yadav <yadavnitya809@gmail.com>
2025-12-07 07:30:52 +0545

    First Commit

---
+++ src/main/java/com/mycompany/pharmastock/PharmaStock.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pharmastock;
+
+/**
+ *
+ * @author nityayadav
+ */
+public class PharmaStock {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
